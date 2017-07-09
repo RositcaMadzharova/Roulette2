b37a0b85b99fa7dd7c924486ecaf730ed877a055
PetyrEftimov <kavazamery@gmail.com>
2017-07-09 16:29:05 +0300

    Update main.cpp
    New version of main.cpp.
    Now each new window is new Background, as required from Vasil/Viktor.
    Each time when user click on button: old button and window are closed and new one is opened.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 /*
+
  * main.cpp
  *
- *  Created on: Jul 8, 2017
- *      Author: kakev
+ *  Created on: Jul 89, 2017
+ *      Author: pepi
  */
 #include "Globals.h"
 #include "Background.h"
@@ -12,15 +13,12 @@
 
 GameState game = INTRO_MENU;
 //INFO,GAME_BOARD,SPIN,OUTRO,WIN
-static int static_counter = 0;
 
 Pools MenuScreen(Credits cr) {
 
-	//faec backgroud
-	if (static_counter == 0) {
-		Background back("Intro Screen", SCREEN_W, SCREEN_H, "chessboard.png");
-	}
-	static_counter++;
+	game = INTRO_MENU;
+
+	Background back("INTRO_MENU", SCREEN_W, SCREEN_H, "chessboard.png");
 
 	//display image
 	LTexture tex1;
@@ -38,23 +36,26 @@ Pools MenuScreen(Credits cr) {
 	p1.setWidth(59);
 	p1.render(Background::gRenderer, &rect);
 
-
-
-//	Pools pG(cr, 300, 330, 10);
-//	p1.loadFromFile(Background::gRenderer, "Pools.png");
-//
-//	SDL_Rect rect = { 118, 1, 118, 111 };
-//	p1.setHeight(59);
-//	p1.setWidth(59);
-//	p1.render(Background::gRenderer, &rect);
-
-
-
-
 	return p1;
 }
 
+void destroyWindow()
+{
+	//Destroy window
+	SDL_DestroyRenderer( Background::gRenderer );
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+	Background::gRenderer = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+    //End destroy window
+
+}
 int main(int argc, char *argv[]) {
+
+
 
 	Credits cr;
 
@@ -62,6 +63,8 @@ int main(int argc, char *argv[]) {
 	p1 = MenuScreen(cr);
 
 	Pools bMainMenu(cr, 400, 400, 10);
+	int counter=0;
+
 
 	bool close = false;
 	while (!close) {
@@ -74,20 +77,23 @@ int main(int argc, char *argv[]) {
 
 				if (game == INTRO_MENU) {
 
-					LTexture tex;
-					tex.loadFromFile(Background::gRenderer,
-							"rouletterules.jpg");
-					SDL_Rect rect2 = { 0, 0, 1600, 1200 };
-					tex.setWidth(800);
-					tex.setHeight(600);
-					tex.render(Background::gRenderer, &rect2);
+					p1.free();
+					destroyWindow();
+					game = INFO;
+
+				}
+
+				if(game == INFO)
+				{
+					Background secondWindow("INFO WINDOW", SCREEN_W, SCREEN_H, "rouletterules.jpg");
+
 
 					bMainMenu.loadFromFile(Background::gRenderer, "Pools.png");
-
 					SDL_Rect rect1 = { 118, 1, 118, 111 };
 					bMainMenu.setHeight(59);
 					bMainMenu.setWidth(59);
 					bMainMenu.render(Background::gRenderer, &rect1);
+
 
 				}
 
@@ -96,13 +102,19 @@ int main(int argc, char *argv[]) {
 			//button that returns to MainMenuPage
 			if (bMainMenu.isClicked(&e)) {
 
-				Pools p2;
-				p2 = MenuScreen(cr);
+				bMainMenu.free();
+				destroyWindow();
+
+				p1 = MenuScreen(cr);
+				game = INTRO_MENU;
+
+
 			}
 
-			if (p1.isHover()) {
-				std::cout << "hovered" << endl;
-			}
+
+//			if (p1.isHover()) {
+//				std::cout << "hovered" << endl;
+//			}
 
 		}
 	}
