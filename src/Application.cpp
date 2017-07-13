c5e803b5761215559d4a546d96c3e5a18356abdb
PetyrEftimov <kavazamery@gmail.com>
2017-07-13 13:06:58 +0300

    Update Application.cpp
    Display all pools in position

--- src/Application.cpp
+++ src/Application.cpp
@@ -2,7 +2,7 @@
  * Application.cpp
  *
  *  Created on: Jul 9, 2017
- *      Author: kakev
+ *      Author: pEftimov
  */
 
 #include "Application.h"
@@ -62,9 +62,11 @@ void Application::initInfo() {
 
 void Application::initGameBoard() {
 	MenuState = GAME_BOARD;
+	//display background
 	gameBoard = new Background("GameBoard", SCREEN_BOARD_W, SCREEN_BOARD_H,
 			"EuropeanRoulette.bmp");
 
+	//display cashout button
 	cashOut = new Button(SCREEN_BOARD_W - BUTTON_W,
 			SCREEN_BOARD_H - BUTTON_H - 10);
 	cashOut->loadFromFile(Background::gRenderer, "Cash OUT.png");
@@ -74,7 +76,6 @@ void Application::initGameBoard() {
 
 	//draw 5 Pulls and PICK PICK >>>>!!!!!!
 	//TODO: Each Poll needs different credit value.
-
 	for (int i = 0; i < POOLS_BUTTON; i++) {
 		gameBoardPools[i] = new Pools(cr, 100 * i, SCREEN_BOARD_H - 60);
 		gameBoardPools[i]->loadFromFile(Background::gRenderer, "Pools.png");
@@ -96,19 +97,27 @@ void Application::initOutro() {
 void Application::initWin() {
 }
 
+//return the number of cell
+//cells: From 0 to 39
+//get parameters: positionX, positionY
 int Application::CalcQuadrandClicked(int x, int y) {
+	//help variable that starts from 0 to 39 in the loops below
 	int sequence = -1;
 	int clickedCell = -1;
 
+	//for 3 lines
 	for (int line = 0; line < 3; line++)
 	{
+		//for each cell in each line
 		for (int i = 0; i < 13; i++) {
 			sequence++;
 
-			//step is 75
-			//start from x=70
-			//start from y=140
-			if (x >= 70 + (75 * i) && x <= (140 + 75 * i) && y >= 280 + (75*line) && y <= 350 + (75*line)) {
+			//step is 75 ; start position of table is from x=70 and y=140
+			if (       x >= 70 + (75 * i)
+					&& x <= (140 + 75 * i)
+					&& y >= 280 + (75*line)
+					&& y <= 350 + (75*line)) {
+
 				clickedCell = sequence;
 			}
 		}
@@ -120,30 +129,31 @@ int Application::CalcQuadrandClicked(int x, int y) {
 
 void Application::DisplayBets(int x, int y, int poolYellow, int poolGreen,
 		int poolRed, int poolBlack, int poolBlue,
-		vector<Point> v_allBetPoints) {
+		vector<Point> v_allBetPoints)
+{
 
 	//choose Yellow Pool for Bet
 	if (poolYellow == 1 && poolGreen == 0 && poolRed == 0 && poolBlack == 0
 			&& poolBlue == 0) {
 
+		//if mouse is on table where possible to bet
 		if (y >= 280 && y <= 510) {
 
 			//TODO:
-			//for each cell 75x75, display in which cell is clicked
-
+			//each cell 75x75
+			//CalcQuadrandClicked(x, y) --> displays and return the number of clicked cell
 			int clickedCell = CalcQuadrandClicked(x, y);
 
 			int coordX = -1;
 			int coordY = -1;
 
-
-
-			//for first line
+			//for all 39 cells
 			for (int i = 0; i <= 39; i++) {
+
 				if (clickedCell == i) {
 
 					//offset/step is 75
-					//10 is because of table fix pixels
+					//10 is hardcoded for izmestvaneto na kartinkata
 					coordX = 70 + (75 * i) + 10;
 
 					//1rst line
@@ -171,7 +181,7 @@ void Application::DisplayBets(int x, int y, int poolYellow, int poolGreen,
 			}
 
 			if (coordX != -1 && coordY != -1) {
-				Credits cr(1);
+				Credits cr(2);
 				Pools gameBoardPools(cr, coordX, coordY);
 				gameBoardPools.loadFromFile(Background::gRenderer, "Pools.png");
 				gameBoardPools.setWidth(PULLS_W);
@@ -251,13 +261,58 @@ void Application::DisplayBets(int x, int y, int poolYellow, int poolGreen,
 
 
 
+
 	if (poolYellow == 0 && poolGreen == 0 && poolRed == 1 && poolBlack == 0
 			&& poolBlue == 0) {
 		cout << "REDD";
 
+
+
+				int clickedCell = CalcQuadrandClicked(x, y);
+
+				int coordX = -1;
+				int coordY = -1;
+
+				//for first line
+				for (int i = 0; i <= 39; i++) {
+					if (clickedCell == i) {
+
+						//offset/step is 75
+						//10 is because of table fix pixels
+						coordX = 70 + (75 * i) + 10;
+
+						//1rst line
+						if (i <= 12) {
+							coordY = 285;
+						}
+
+						//2nd line
+						if (i > 12 && i<= 25) {
+							coordX = 70 + (75 * (i-13)) + 10;
+
+							cout << "coordX"<< coordX;
+							coordY = 360;
+						}
+
+						//3rd line
+						if (i >= 26 && i<= 39) {
+							coordX = 70 + (75 * (i-26)) + 10;
+
+							cout << "3coordX"<< coordX;
+
+							coordY = 445;
+						}
+					}
+				}
+
+		//TODO
+		//from :   Pools gameBoardPools(cr, x, y);
+		//to   :  Pools gameBoardPools(cr, coordX, coordY);
+
+
 		if (y >= 280 && y <= 510) {
 			Credits cr(20);
-			Pools gameBoardPools(cr, x, y);
+			Pools gameBoardPools(cr, coordX, coordY);
 			gameBoardPools.loadFromFile(Background::gRenderer, "Pools.png");
 			gameBoardPools.setWidth(PULLS_W);
 			gameBoardPools.setHeight(PULLS_H);
@@ -272,11 +327,52 @@ void Application::DisplayBets(int x, int y, int poolYellow, int poolGreen,
 	if (poolYellow == 0 && poolGreen == 0 && poolRed == 0 && poolBlack == 1
 			&& poolBlue == 0) {
 
+
+		//TODO
+		//TODO
+				//from :   Pools gameBoardPools(cr, x, y);
+				//to   :  Pools gameBoardPools(cr, coordX, coordY);
 		cout << "BLACK";
+		int clickedCell = CalcQuadrandClicked(x, y);
+
+		int coordX = -1;
+		int coordY = -1;
+
+		//for first line
+		for (int i = 0; i <= 39; i++) {
+			if (clickedCell == i) {
+
+				//offset/step is 75
+				//10 is because of table fix pixels
+				coordX = 70 + (75 * i) + 10;
+
+				//1rst line
+				if (i <= 12) {
+					coordY = 285;
+				}
+
+				//2nd line
+				if (i > 12 && i<= 25) {
+					coordX = 70 + (75 * (i-13)) + 10;
+
+					cout << "coordX"<< coordX;
+					coordY = 360;
+				}
+
+				//3rd line
+				if (i >= 26 && i<= 39) {
+					coordX = 70 + (75 * (i-26)) + 10;
+
+					cout << "3coordX"<< coordX;
+
+					coordY = 445;
+				}
+			}
+		}
 
 		if (y >= 280 && y <= 510) {
 			Credits cr(100);
-			Pools gameBoardPools(cr, x, y);
+			Pools gameBoardPools(cr, coordX, coordY);
 			gameBoardPools.loadFromFile(Background::gRenderer, "Pools.png");
 			gameBoardPools.setWidth(PULLS_W);
 			gameBoardPools.setHeight(PULLS_H);
@@ -292,10 +388,50 @@ void Application::DisplayBets(int x, int y, int poolYellow, int poolGreen,
 			&& poolBlue == 1) {
 
 		cout << "BLue";
+		int clickedCell = CalcQuadrandClicked(x, y);
+
+		int coordX = -1;
+		int coordY = -1;
+
+		//for first line
+		for (int i = 0; i <= 39; i++) {
+			if (clickedCell == i) {
+
+				//offset/step is 75
+				//10 is because of table fix pixels
+				coordX = 70 + (75 * i) + 10;
+
+				//1rst line
+				if (i <= 12) {
+					coordY = 285;
+				}
+
+				//2nd line
+				if (i > 12 && i<= 25) {
+					coordX = 70 + (75 * (i-13)) + 10;
+
+					cout << "coordX"<< coordX;
+					coordY = 360;
+				}
+
+				//3rd line
+				if (i >= 26 && i<= 39) {
+					coordX = 70 + (75 * (i-26)) + 10;
+
+					cout << "3coordX"<< coordX;
+
+					coordY = 445;
+				}
+			}
+		}
+
+		//TODO
+				//from :   Pools gameBoardPools(cr, x, y);
+				//to   :  Pools gameBoardPools(cr, coordX, coordY);
 
 		if (y >= 280 && y <= 510) {
 			Credits cr(50);
-			Pools gameBoardPools(cr, x, y);
+			Pools gameBoardPools(cr, coordX, coordY);
 			gameBoardPools.loadFromFile(Background::gRenderer, "Pools.png");
 			gameBoardPools.setWidth(PULLS_W);
 			gameBoardPools.setHeight(PULLS_H);
