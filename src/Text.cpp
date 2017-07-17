c72fe20be095d0466e15ed0775449ffd0ea89bd8
VasilKakevski <kakevski@abv.bg>
2017-07-18 02:20:34 +0300

    Add files via upload
    u need to remove everything from background exept TTF_INIT() and magic happens

---
+++ src/Text.cpp
@@ -0,0 +1,52 @@
+/*
+ * Text.cpp
+ *
+ *  Created on: Jul 17, 2017
+ *      Author: kakev
+ */
+
+#include "Text.h"
+
+Text::Text(int x,int y,int w,int h,
+		int front_size,
+		 std::string massage_text,
+		 SDL_Color color)
+{
+	TTF_Init();
+	textRect= new SDL_Rect;
+	textRect->w=w;
+	textRect->h=h;
+	textRect->x=x;
+	textRect->y=y;
+	textTexture=loadFromFile(front_size,massage_text,color);
+}
+
+
+
+Text::~Text()
+{
+	TTF_Quit();
+	delete textRect;
+	SDL_DestroyTexture(textTexture);
+}
+
+SDL_Texture* Text::loadFromFile(
+		int front_size,
+		 std::string massage_text,
+		 SDL_Color color) {
+
+	TTF_Font* gFont =TTF_OpenFont("lazy.ttf", front_size) ;
+
+	SDL_Surface* textSurface = TTF_RenderText_Solid(gFont, massage_text.c_str(),
+			color);
+	SDL_Texture *textTexture = SDL_CreateTextureFromSurface(Background::gRenderer,
+			textSurface);
+	SDL_RenderCopy(Background::gRenderer,textTexture,NULL,textRect);
+	SDL_RenderPresent(Background::gRenderer);
+	//Get rid of old surface
+	SDL_FreeSurface(textSurface);
+	//Return success
+	return textTexture;
+}
+
+
---
+++ src/Text.h
@@ -0,0 +1,33 @@
+/*
+ * Text.h
+ *
+ *  Created on: Jul 17, 2017
+ *      Author: kakev
+ */
+
+#ifndef TEXT_H_
+#define TEXT_H_
+#include "Globals.h"
+#include "Background.h"
+
+class Text {
+public:
+	Text(int x, int y, int w,int h,
+			int front_size,
+			 std::string massage_text,
+			 SDL_Color color );
+	virtual ~Text();
+	SDL_Texture * loadFromFile(
+			int front_size,
+			 std::string massage_text,
+			 SDL_Color color);
+	SDL_Texture * getTexture (){ return textTexture;}
+
+
+
+private:
+	SDL_Rect * textRect;
+	SDL_Texture *textTexture;
+};
+
+#endif /* TEXT_H_ */
